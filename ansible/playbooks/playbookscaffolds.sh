75aee2d35037ef2c06c189bd806b2c53ec4d06af
Sam <samriddhac@gmail.com>
2017-01-09 13:45:14 +0000

    Ansible--playbook--v2

--- ansible/playbooks/playbookscaffolds.sh
+++ ansible/playbooks/playbookscaffolds.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 #Display usage informatiosn
-usage() { echo "Usage: $0 [-p <Playbook Path> ] [-t <Playbook Title> ]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-p <Playbook Path> ] [-t <Playbook Title> ]" 2>&1; exit 1; }
 [ $# -eq 0 ] && usage
+#Side Note 
+# File descriptor 0 --> stdin
+# File descriptor 1 --> stdout
+# File descriptor 2 --> stderr
+# 2>&1 --> dump content of stderr to stdout.
 
+#getopts --> Get the options
+
+#End side note
 while getopts ":p:t:" OPTION; do
 	case ${OPTION} in
 		p)
@@ -17,3 +25,31 @@ while getopts ":p:t:" OPTION; do
 			;;
 	esac
 done
+
+if [ -z ${PROJECT_PATH} ]; then 
+	echo "Please supply a project path "
+	exit 1
+fi
+if [ -z ${PLAYBOOK_TITLE} ]; then
+        echo "Please supply a playbook title "
+        exit 1
+fi
+
+# Create the basic folder structure
+
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/files
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/group_vars
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/host_vars/dev
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/host_vars/qa
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/inventories
+mkdir -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/roles
+
+#Create default common roles
+ansible-galaxy init common -p ${PROJECT_PATH}/${PLAYBOOK_TITLE}/roles
+
+touch ${PROJECT_PATH}/${PLAYBOOK_TITLE}/inventories/dev
+touch ${PROJECT_PATH}/${PLAYBOOK_TITLE}/inventories/qa
+
+touch ${PROJECT_PATH}/${PLAYBOOK_TITLE}/${PLAYBOOK_TITLE}.yml
+
+
